901cd1d0eb0cd6f3e4c50c749985c3c226eb7043
De.Yonis <y.abdillahi03@gmail.com>
2021-03-10 16:56:43 +0000

    doc:added testing file
    Updated extract_and_remove_sensitive_data function

--- src/app.py
+++ src/app.py
@@ -1,24 +1,22 @@
 import csv
 
-data = []      
 def extract_and_remove_sensitive_data():
+    data = [] 
     print("Extracting...")
     try:
         with open('/workspace/2021-02-23-isle-of-wight.csv', 'r') as file:
-            source_file = csv.DictReader(file, fieldnames=['date_time','location','full_name','order','payment_type','amount','card_details'], delimiter=',')
+            fieldnames=['date_time','location','full_name','order','payment_type','amount','card_details']
+            source_file = csv.DictReader(file, fieldnames = fieldnames, delimiter=',')
             next(source_file) #ignore the header row
             for row in source_file:
-                data.append((row['date_time'], row['location'], row['full_name'], row['order'], row['payment_type'], row['amount']))
+                row = dict(row)
+                if 'card_details' in row:
+                    del row['card_details']
+                data.append(row)
     except Exception as error:
         print("An error occurred: " + str(error))
-
     return data
-
 extract_and_remove_sensitive_data()
 
-for i in data:
+for i in extract_and_remove_sensitive_data():
     print(i)
-
-
-
-
