a9181db8715e0f7682714f5cf58be0b5f980357a
mnofonagoro <monica.ofonagoro@googlemail.com>
2021-03-14 14:13:55 +0000

    updated postgres script and separated connection function

---
+++ src/connecting_to_db.py
@@ -0,0 +1,11 @@
+import psycopg2
+
+def create_db_connection():
+    connection = psycopg2.connect(
+                    database="team-3-group-project",
+                    user="root", 
+                    password="password", 
+                    host="172.18.0.2", 
+                    port="5432"
+                    )
+    return connection
--- src/sql_script.py
+++ src/sql_script.py
@@ -1,30 +1,15 @@
-import psycopg2
+from connecting_to_db import create_db_connection
 
 # When I opened up my container, my IP address had changed, so this is the code to check it:
 # docker inspect team-3-project_devcontainer_postgres_1 | grep IPAddress
 
-# CREATE TYPE size AS ENUM ('Standard' , 'Regular' , 'Large');
+connection = create_db_connection()
 
-def create_db_connection():
-    connection = psycopg2.connect(
-                    database="team-3-group-project",
-                    user="root", 
-                    password="password", 
-                    host="172.18.0.2", 
-                    port="5432"
-                    )
-    return connection
-
-def create_product_size_table():
+def create_product_size_type():
     try:
-        connection = create_db_connection()
         with connection.cursor() as cursor:
-            sql = '''CREATE TABLE IF NOT EXISTS product_size(
-                        product_size_id SERIAL PRIMARY KEY,
-                        product_size VARCHAR(50) NOT NULL
-                        );
-                        INSERT INTO product_size(product_size)
-                        VALUES('standard'), ('regular'), ('large')'''
+            sql = '''CREATE TYPE product_size AS ENUM 
+                    ('Standard' , 'Regular' , 'Large')'''
             cursor.execute(sql)        
             connection.commit()
     except Exception as e:
@@ -32,14 +17,12 @@ def create_product_size_table():
 
 def create_product_table():
     try:
-        connection = create_db_connection()
         with connection.cursor() as cursor:
             sql = '''CREATE TABLE IF NOT EXISTS product(
                         product_id SERIAL PRIMARY KEY,
                         product_name VARCHAR(100) NOT NULL,
-                        product_size_id INT,
-                        product_price FLOAT,
-                        CONSTRAINT fk_product_size FOREIGN KEY(product_size_id) REFERENCES product_size(product_size_id)
+                        product_size product_size,
+                        product_price FLOAT
                         )'''
             cursor.execute(sql)
             connection.commit() 
@@ -48,7 +31,6 @@ def create_product_table():
 
 def create_branch_table():
     try:
-        connection = create_db_connection()
         with connection.cursor() as cursor:
             sql = '''CREATE TABLE IF NOT EXISTS branch(
                         branch_id SERIAL PRIMARY KEY,
@@ -61,7 +43,6 @@ def create_branch_table():
 
 def create_transaction_table():
     try:
-        connection = create_db_connection()
         with connection.cursor() as cursor:
             sql = '''CREATE TABLE IF NOT EXISTS transaction(
                         transaction_id SERIAL PRIMARY KEY,
@@ -77,7 +58,6 @@ def create_transaction_table():
 
 def create_basket_table():
     try:
-        connection = create_db_connection()
         with connection.cursor() as cursor:
             sql = '''CREATE TABLE IF NOT EXISTS basket(
                         basket_id SERIAL PRIMARY KEY,
@@ -91,10 +71,13 @@ def create_basket_table():
     except Exception as e:
         print(e)
 
-create_product_size_table()
+def close_connection():
+    return connection.close()
+
+create_product_size_type()
 create_product_table()
 create_branch_table()
 create_transaction_table()
 create_basket_table()
-
+close_connection()
 print("Your tables have been created successfully")
